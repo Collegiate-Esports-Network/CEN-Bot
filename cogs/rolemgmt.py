25ff70991ce62d611058aee016fc5f4d432733ee
Justin Panchula <Justin.Panchula@gmail.com>
2022-05-04 18:09:14 -0400

    finalized

--- cogs/rolemgmt.py
+++ cogs/rolemgmt.py
@@ -38,8 +38,8 @@ class rolemgmt(commands.Cog):
         await ctx.send('')
 
     # Add reactions to file
-    @commands.command(name='addreact')
-    async def addreact(self, ctx):
+    @commands.command(name='reactadd')
+    async def reactadd(self, ctx):
         # Init
         path = Path.cwd()
 
@@ -58,33 +58,66 @@ class rolemgmt(commands.Cog):
             roles = dict()
 
         # Check if command user is giving input
-        def check(user):
+        def checkuser(user):
             return user.author == ctx.author and user.channel == ctx.channel
 
         # Prompts for user entry
-        Qs = ['Role Category', 'Role Name', 'Role Emoji']
-        newrole = {}
+        Qs = ['Role Category', 'Role Name']
+        newrole = dict()
 
         # Ask questions and get answers
         for Q in Qs:
             await ctx.send(Q + '?')
 
             try:
-                msg = await self.bot.wait_for('message', timeout=30.0, check=check)
+                msg = await self.bot.wait_for('message', timeout=30.0, check=checkuser)
             except asyncio.TimeoutError:
                 await ctx.send('Command has timed out')
                 return
             else:
                 newrole[Q] = msg.content
 
-        # Validates that role reaction does not exist yet
+        # Check for new category
         try:
-            roles[newrole[Qs[1]]]
+            roles[newrole[Qs[0]]]
         except KeyError:
-            roles[newrole[Qs[1]]] = {Qs[0]: newrole[Qs[0]], Qs[2]: newrole[Qs[2]]}
-        else:
-            await ctx.send('Role Reaction already exists!')
-            return
+            # Ask for category description
+            await ctx.send('New role category created. What is the category description?')
+            try:
+                msg = await self.bot.wait_for('message', timeout=60.0, check=checkuser)
+            except asyncio.TimeoutError:
+                await ctx.send('Command has timed out')
+                return
+            else:
+                cDescription = msg.content
+                roles[newrole[Qs[0]]] = {'Description': cDescription, 'Roles': {}}
+        finally:
+            # Check for new role
+            try:
+                roles[newrole[Qs[0]]]['Roles'][newrole[Qs[1]]]
+            except KeyError:
+                # Ask for role description and emoji
+                await ctx.send('New role reaction created. What is the role description?')
+                try:
+                    msg1 = await self.bot.wait_for('message', timeout=60.0, check=checkuser)
+                except asyncio.TimeoutError:
+                    await ctx.send('Command has timed out')
+                    return
+                else:
+                    await ctx.send('What is the role emoji?')
+                    try:
+                        msg2 = await self.bot.wait_for('message', timeout=60.0, check=checkuser)
+                    except asyncio.TimeoutError:
+                        await ctx.send('Command has timed out')
+                        return
+                    else:
+                        # Populate
+                        rDescription = msg1.content
+                        emoji = msg2.content
+                        roles[newrole[Qs[0]]]['Roles'][newrole[Qs[1]]] = {'Description': rDescription, "Emoji": emoji}
+            else:
+                await ctx.send('That role reaction already exists!')
+                return
 
         # Dump into roles.json
         write_json(path, roles)
