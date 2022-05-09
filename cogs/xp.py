22ab044555cfe95e2f6a09a578b87258b9263d7a
Justin Panchula <Justin.Panchula@gmail.com>
2022-05-09 14:09:31 -0400

    xp started

---
+++ cogs/xp.py
@@ -0,0 +1,16 @@
+__author__ = 'Justin Panchula'
+__copyright__ = 'Copyright 2022'
+__credits__ = 'Justin Panchula, Zach Lesniewski'
+__license__ = 'MIT License'
+__version__ = '1.0.0'
+__status__ = 'Production'
+__doc__ = """xp Functions"""
+
+# Python imports
+import logging
+
+# Discord imports
+from discord.ext import commands
+
+# Cutom imports
+from utils import JsonInteracts
